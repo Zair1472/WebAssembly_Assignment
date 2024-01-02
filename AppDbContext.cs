6107c7287a0ae9b85dff98fb00acaaecdcc2fa64
Zair1472 <150200144+Zair1472@users.noreply.github.com>
2024-01-03 03:43:34 +0500

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebAssembly_Assignment.Shared;
+namespace WebAssembly_Assignment.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+       
+        public DbSet<Donation> Donations { get; set; }
+
+    }
+}
