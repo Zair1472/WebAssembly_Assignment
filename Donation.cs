8f3418656cb06752577c81ec159b6cf1708c87f0
Zair1472 <150200144+Zair1472@users.noreply.github.com>
2024-01-03 03:45:32 +0500

    Add files via upload

---
+++ Donation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAssembly_Assignment.Shared
+{
+    public class Donation
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string MedicineName { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public string Potency { get; set; }
+        [Required]
+        public int DonateQuantity { get; set; }
+    }
+}
