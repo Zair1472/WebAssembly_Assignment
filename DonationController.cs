e7724092dc521882cdc8657ebba585a529ee6ecb
Zair1472 <150200144+Zair1472@users.noreply.github.com>
2024-01-03 03:42:37 +0500

    Add files via upload

---
+++ DonationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAssembly_Assignment.Server.Data;
+using WebAssembly_Assignment.Shared;
+
+namespace WebAssembly_Assignment.Server.Controllers
+{
+    
+        [Route("api/v1/[controller]")]
+        [ApiController]
+        public class DonationController : ControllerBase
+        {
+
+            AppDbContext _appDbContext;
+
+            public DonationController(AppDbContext appDbContext)
+            {
+                _appDbContext = appDbContext;
+            }
+
+            [HttpGet("{id}")]
+            public string Get(int id)
+            {
+                return "value";
+            }
+
+
+            [HttpPost]
+            public IActionResult Post([FromBody] Donation donation)
+            {
+                try
+                {
+                    _appDbContext.Donations.Add(donation);
+                    _appDbContext.SaveChanges();
+
+                    return Ok("Message:Save Successfuly");
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            }
+
+
+            [HttpPut("{Id}")]
+            public void Put(int Id, [FromBody] string value)
+            {
+            }
+
+
+        }
+    }
